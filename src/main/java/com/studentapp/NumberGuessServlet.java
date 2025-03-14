45c6b91ab54009d56885b0088e3f74fcc8c31168
EC2 Default User <ec2-user@ip-172-31-6-156.us-east-2.compute.internal>
2025-03-14 02:45:14 +0000

    UPDATED GUESS SERVLET

--- src/main/java/com/studentapp/NumberGuessServlet.java
+++ src/main/java/com/studentapp/NumberGuessServlet.java
@@ -4,20 +4,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/guess")
 public class NumberGuessServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private int targetNumber;
-    
+
     public void init() throws ServletException {
         targetNumber = new Random().nextInt(100) + 1;
     }
-    
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
@@ -28,20 +26,25 @@ public class NumberGuessServlet extends HttpServlet {
         out.println("<input type='submit' value='Submit' />");
         out.println("</form>");
     }
-    
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        int guess = Integer.parseInt(request.getParameter("guess"));
-        if (guess < targetNumber) {
-            out.println("<h2>Your guess is too low. Try again!</h2>");
-        } else if (guess > targetNumber) {
-            out.println("<h2>Your guess is too high. Try again!</h2>");
-        } else {
-            out.println("<h2>Congratulations! You guessed the number!</h2>");
-            targetNumber = new Random().nextInt(100) + 1;
+        try {
+            int guess = Integer.parseInt(request.getParameter("guess"));
+            if (guess < targetNumber) {
+                out.println("<h2>Your guess is too low. Try again!</h2>");
+            } else if (guess > targetNumber) {
+                out.println("<h2>Your guess is too high. Try again!</h2>");
+            } else {
+                out.println("<h2>Congratulations! You guessed the number!</h2>");
+                targetNumber = new Random().nextInt(100) + 1; // Reset game
+            }
+        } catch (NumberFormatException e) {
+            out.println("<h2>Invalid input. Please enter a valid number.</h2>");
         }
         out.println("<a href='guess'>Play Again</a>");
     }
 }
+
